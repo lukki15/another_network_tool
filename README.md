6a18a8fdb9b4db5b4076d9ee82e70d268cfcde22
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-12-21 05:57:46 +0000

    feat: :tada: add an empty flutter application
    A flutter application without descriptions comments or tests.

---
+++ README.md
@@ -0,0 +1,3 @@
+# network_info_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/network_info_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.network_info_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
