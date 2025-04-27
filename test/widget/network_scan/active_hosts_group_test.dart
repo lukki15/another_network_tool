ef752e711331bc01fc8e6e3a2a11b0c15a9a5e09
lukki15 <50550773+lukki15@users.noreply.github.com>
2025-04-27 16:15:12 +0000

    test: :white_check_mark: add unit test for the ActiveHostsGroup widget
    note that the on long press unit test is unfinished

---
+++ test/widget/network_scan/active_hosts_group_test.dart
@@ -0,0 +1,101 @@
+import 'package:another_network_tool/widget/network_scan/active_hosts_group.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:network_tools/network_tools.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:forui/forui.dart';
+
+@GenerateNiceMocks([MockSpec<ActiveHost>()])
+@GenerateNiceMocks([MockSpec<NavigatorObserver>()])
+import './active_hosts_group_test.mocks.dart';
+
+void main() {
+  group('ActiveHostsGroup Tests', () {
+    Future<MockNavigatorObserver> pumpActiveHostsGroup(
+      WidgetTester t,
+      Set<ActiveHost> activeHosts,
+    ) async {
+      final mockObserver = MockNavigatorObserver();
+      await t.pumpWidget(
+        MaterialApp(
+          navigatorObservers: [mockObserver],
+          home: ActiveHostsGroup(activeHosts: activeHosts),
+        ),
+      );
+
+      await t.pumpAndSettle();
+      return mockObserver;
+    }
+
+    MockActiveHost makeMockActiveHost() {
+      var activeHost = MockActiveHost();
+      when(activeHost.deviceName).thenAnswer((_) => Future.value("deviceName"));
+      when(activeHost.address).thenAnswer((_) => "address");
+      when(activeHost.vendor).thenAnswer(
+        (_) => Future.value(
+          Vendor(
+            macPrefix: "macPrefix",
+            vendorName: "vendorName",
+            private: "private",
+            blockType: "blockType",
+            lastUpdate: "lastUpdate",
+          ),
+        ),
+      );
+
+      return activeHost;
+    }
+
+    testWidgets('without active hosts', (WidgetTester t) async {
+      await pumpActiveHostsGroup(t, {});
+
+      expect(find.byType(FTile), findsNothing);
+    });
+
+    testWidgets('with one active host', (WidgetTester t) async {
+      await pumpActiveHostsGroup(t, {makeMockActiveHost()});
+
+      expect(find.byType(FTile), findsOneWidget);
+      expect(find.text("deviceName"), findsOneWidget);
+      expect(find.text("address"), findsOneWidget);
+      expect(find.text("vendorName"), findsOneWidget);
+    });
+
+    testWidgets('with two active host', (WidgetTester t) async {
+      await pumpActiveHostsGroup(t, {
+        makeMockActiveHost(),
+        makeMockActiveHost(),
+      });
+
+      expect(find.byType(FTile), findsNWidgets(2));
+      expect(find.text("deviceName"), findsNWidgets(2));
+      expect(find.text("address"), findsNWidgets(2));
+      expect(find.text("vendorName"), findsNWidgets(2));
+    });
+
+    testWidgets('on press', (WidgetTester t) async {
+      var mockObserver = await pumpActiveHostsGroup(t, {makeMockActiveHost()});
+
+      var tile = find.byType(FTile);
+      await t.press(tile);
+      await t.pumpAndSettle();
+
+      verify(mockObserver.didPush(any, any)).called(1);
+    });
+
+    testWidgets('on long press', (WidgetTester t) async {
+      await pumpActiveHostsGroup(t, {makeMockActiveHost()});
+
+      var tile = find.byType(FTile);
+      await t.longPress(tile);
+      await t.pumpAndSettle();
+
+      // TODO: extend test
+      // expect(await Clipboard.hasStrings(), true);
+      // var clipBoardData = await Clipboard.getData(Clipboard.kTextPlain);
+      // expect(clipBoardData!.text!, "address");
+    });
+  });
+}
