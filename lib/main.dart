d26a047b49586b9e5f27dd791123f98341d0ac6b
lukki15 <50550773+lukki15@users.noreply.github.com>
2026-02-28 20:20:56 +0000

    refactor: :lipstick: improve Application widget structure and theme handling

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,38 @@ class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    builder: (context, child) => FTheme(
-      data: FThemes.zinc.light,
-      child: child!,
-    ),
-    home: LoadingFutureBuilder<Directory>(
-      future: getDirectory(),
-      loadingMessage: "get directory",
-      onData: (directory) => SetupNetworkTools(directory: directory),
-    ),
-  );
+  Widget build(BuildContext context) {
+    /// Try changing this and hot reloading the application.
+    ///
+    /// To create a custom theme:
+    /// ```shell
+    /// dart forui theme create [theme template].
+    /// ```
+    final theme = FThemes.zinc.light;
+    return MaterialApp(
+      // TODO: replace with your application's supported locales.
+      supportedLocales: FLocalizations.supportedLocales,
+      // TODO: add your application's localizations delegates.
+      localizationsDelegates: const [...FLocalizations.localizationsDelegates],
+      // MaterialApp's theme is also animated by default with the same duration and curve.
+      // See https://api.flutter.dev/flutter/material/MaterialApp/themeAnimationStyle.html
+      // for how to configure this.
+      //
+      // There is a known issue with implicitly animated widgets where their transition
+      // occurs AFTER the theme's. See https://github.com/duobaseio/forui/issues/670.
+      theme: theme.toApproximateMaterialTheme(),
+      builder: (_, child) => FTheme(
+        data: theme,
+        child: FToaster(child: FTooltipGroup(child: child!)),
+      ),
+      // You can also replace FScaffold with Material Scaffold.
+      home: LoadingFutureBuilder<Directory>(
+        future: getDirectory(),
+        loadingMessage: "get directory",
+        onData: (directory) => SetupNetworkTools(directory: directory),
+      ),
+    );
+  }
 }
 
 class SetupNetworkTools extends StatelessWidget {
